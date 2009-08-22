62f2bd3f75adb02e4601a04a9d14d0a1cd5ee37e
John Miller <john.miller@greenfieldstudios.net>
2009-08-22 15:26:11 -0400

    added error handling to the application controller for unauthorized access attempts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  rescue_from Twitter::Unauthorized, :with => :twitter_unauthorized
+
+  private
+    def twitter_unauthorized(exception)
+      flash[:notice] = "Unathorized. Please log in again."
+      redirect_to :controller => :home, :action => :index
+    end
 end

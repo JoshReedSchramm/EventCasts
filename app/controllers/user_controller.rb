a1f838a04769bcfc7554665848db41e64578944f
John Miller <john.miller@greenfieldstudios.net>
2009-08-22 14:44:33 -0400

    fixed yet another bug in user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,7 +13,7 @@ class UserController < ApplicationController
     verification_response = verify_credentials
     clear_request_token
 
-    unless verification_response = Net::HTTPSuccess
+    unless verification_response == Net::HTTPSuccess
       handle_failed_authorization
       return
     end

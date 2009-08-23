f0dea175031c0871aa850b16e07aa18022ef5d8d
John Miller <john.miller@greenfieldstudios.net>
2009-08-23 10:36:51 -0400

    add header to search results. linked twitter ids to twitter.com/user

--- public/javascripts/groups.js
+++ public/javascripts/groups.js
@@ -67,7 +67,7 @@ function update_tweet_results(tweets, first) {
     $.each(tweets, function(count, tweet){
         tmp = $("#tweetTemplate").clone()
 		tmp.attr("id", "tweet_"+tweet.id);
-        $(".tweeter", tmp).html(tweet.from_user);
+        $(".tweeter", tmp).html("<a href='http://twitter.com/" + tweet.from_user + "' target='_blank'>@"+ tweet.from_user + "</a>");
         $(".tweet", tmp).html(tweet.text);
 		$(".profile_image", tmp).attr("src", tweet.profile_image_url);
 		$(".update_stamp", tmp).html(distanceOfTimeInWords(new Date(), new Date(tweet.created_at), false)+" ago from "+html_entity_decode(tweet.source));

c037ae9d354f7469e4f7d8b2d86c4eace65fe68a
John Miller <john.miller@greenfieldstudios.net>
2009-08-28 19:01:06 -0400

    Fixed syntax error in search_result_spec which was keeping tests from running

---
+++ spec/models/search_result_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe SearchResult do
+  it "should be able to create a new search result instance" do
+    SearchResult.new
+  end
+end

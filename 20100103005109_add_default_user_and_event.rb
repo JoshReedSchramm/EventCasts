93e38a4307f19e22671a37a83e14608ea3d61276
Josh Schramm <josh.schramm@gmail.com>
2010-01-03 18:16:43 -0500

    updated migration to occur after search terms are created

--- 20100103005109_add_default_user_and_event.rb
+++ 20100103005109_add_default_user_and_event.rb
@@ -2,9 +2,9 @@ class AddDefaultUserAndEvent < ActiveRecord::Migration
   def self.up
     default_account = User.create(:username=>'asktwoups', :password=>'password')
     default_account.save!
-    
+        
     event = Event.create(:name=>'twoups')
-    event.save!
+    event.save
     
     default_account.events << event
     default_account.save!

d834c62b75acfefdb40f16d676e7632c2464cdc2
Josh Schramm <josh.schramm@gmail.com>
2010-02-03 15:41:46 -0500

    Removed old non-used javascript code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,44 +1,52 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function() {
-	if ($('#search_box').val() != "Find an event or group") {
-		$('#search_box').css('color', 'black');
-	}
-  	$('#search_button').click(function(){
-		if ($('#search_box').val() == "Find an event or group") {
-			$('#search_box').val("");
-		}
-		$('#search_form').submit();
-	});
-	$('#search_box').focus(function() {
-		if ($('#search_box').val() == "Find an event or group") {
-			$('#search_box').css('color', 'black');
-			$('#search_box').val("");
-		}
-	});
-	$('#search_box').blur(function() {
-		if ($('#search_box').val() == "") {
-			$('#search_box').css('color', 'gray');
-			$('#search_box').val("Find an event or group");
-		}
-	});
-	$('#search_form').ajaxForm({
-	    success: show_search_results
-	});
-	$("#add_group_link").click(function(){
-		$("#cancel_add_group_link").show();
-		$("#add_group_link").hide();
-		$("#add_group_form").show();
-		$("#add_group_box").focus();
-	});
-	$("#cancel_add_group_link").click(function(){
-		$(".error_message").remove();
+function display_error_on(fieldId, error) {
+	$(".error_message").remove();
+	
+	$("#"+fieldId).parent().after("<div class='error_message' id='error_"+fieldId+"'>"+error+"</div>");
+	var parent_pos = $("#"+fieldId).position();
+}
 
-		$("#cancel_add_group_link").hide();
-		$("#add_group_link").show();
-		$("#add_group_form").hide();
-	});
-	$("#save_group_link").click(function(){
-		$('#add_group_form').submit();
-	});
-});
+function html_entity_decode(str) {
+  var ta=document.createElement("textarea");
+  ta.innerHTML=str.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+  return ta.value;
+}
+
+function distanceOfTimeInWords(fromTime, toTime, includeSeconds) {
+  var fromSeconds = fromTime.getTime();
+  var toSeconds = toTime.getTime();
+  var distanceInSeconds = Math.round(Math.abs(fromSeconds - toSeconds) / 1000)
+  var distanceInMinutes = Math.round(distanceInSeconds / 60)
+  if (distanceInMinutes <= 1) {
+    if (!includeSeconds)
+      return (distanceInMinutes == 0) ? 'less than a minute' : '1 minute'
+    if (distanceInSeconds < 5)
+      return 'less than 5 seconds'
+    if (distanceInSeconds < 10)
+      return 'less than 10 seconds'
+    if (distanceInSeconds < 20)
+      return 'less than 20 seconds'
+    if (distanceInSeconds < 40)
+      return 'half a minute'
+    if (distanceInSeconds < 60)
+      return 'less than a minute'
+    return '1 minute'
+  }
+  if (distanceInMinutes < 45)
+    return distanceInMinutes + ' minutes'
+  if (distanceInMinutes < 90)
+    return "about 1 hour" 
+  if (distanceInMinutes < 1440)
+    return "about " + (Math.round(distanceInMinutes / 60)) + ' hours'
+  if (distanceInMinutes < 2880)
+    return "1 day" 
+  if (distanceInMinutes < 43200)
+    return (Math.round(distanceInMinutes / 1440)) + ' days'
+  if (distanceInMinutes < 86400)
+    return "about 1 month" 
+  if (distanceInMinutes < 525600)
+    return (Math.round(distanceInMinutes / 43200)) + ' months'
+  if (distanceInMinutes < 1051200)
+    return "about 1 year" 
+  return "over " + (Math.round(distanceInMinutes / 525600)) + ' years'
+}
+
